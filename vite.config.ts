bc856a52fd624b3bce53998310e32461dac7207e
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2025-10-14 19:09:21 +0530

    Added vercel config-7

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
+import { reactRouter } from "@react-router/dev/vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import vercel from "vite-plugin-vercel";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), vercel()],
+  plugins: [react(), tailwindcss(), vercel(), reactRouter()],
   base: "/",
 });
